ee53ae11f9d159bf069b80752b0e136a81750554
zhb666 <462612421@qq.com>
2019-03-29 12:03:25 +0800

    test

---
+++ js/rem.js
@@ -0,0 +1,27 @@
+fnResize()
+
+window.onresize = function () {
+
+  fnResize()
+
+}
+
+function fnResize() {
+
+  var deviceWidth = document.documentElement.clientWidth || window.innerWidth
+
+  if (deviceWidth >= 750) {
+
+    deviceWidth = 750
+
+  }
+
+  if (deviceWidth <= 320) {
+
+    deviceWidth = 320
+
+  }
+
+  document.documentElement.style.fontSize = (deviceWidth / 7.5*2) + 'px'
+
+}
